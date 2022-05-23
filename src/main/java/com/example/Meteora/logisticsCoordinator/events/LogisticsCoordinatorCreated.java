b6fae1e7b99154a6ada6ade9e9b03fdfc30919e1
julytatiana <yulytatiana@live.com>
2022-05-22 23:18:30 -0500

    LogisticsCoordinatorCreated file updated

--- src/main/java/com/example/Meteora/logisticsCoordinator/events/LogisticsCoordinatorCreated.java
+++ src/main/java/com/example/Meteora/logisticsCoordinator/events/LogisticsCoordinatorCreated.java
@@ -1,6 +1,7 @@
 package com.example.Meteora.logisticsCoordinator.events;
 
 import co.com.sofka.domain.generic.DomainEvent;
+import com.example.Meteora.accountant.values.AccountantName;
 import com.example.Meteora.logisticsCoordinator.values.CoordinatorName;
 
 public class LogisticsCoordinatorCreated extends DomainEvent {
@@ -8,10 +9,16 @@ public class LogisticsCoordinatorCreated extends DomainEvent {
     //private parameter
     private final CoordinatorName coordinatorName;
 
+    //Constructor
+    //Between parentesis is a property that will transport from one place to another
     public LogisticsCoordinatorCreated(CoordinatorName coordinatorName){
+        //it brings a type, the domain is meteora, accountantcreated is the specific type
         super("meteora.logisticscoordinator.logisticscoordinatorcreated");
         this.coordinatorName = coordinatorName;
     }
+
+    //Get LOOK FOR A SHORCUT!
+    //Flat Object, it's not an entity, it's only to transport objects like a DTO
 
     public CoordinatorName getCoordinatorName() {
         return coordinatorName;
